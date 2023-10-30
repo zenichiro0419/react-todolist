4f25558f25ddf18bc1c0210416a9ee55c4057bb7
Zenichiro Ishikawa <37606032+zenichiro0419@users.noreply.github.com>
2023-10-30 18:33:06 +0000

    TODO数に個数制限を設定(2)

--- src/components/InputTodo.jsx
+++ src/components/InputTodo.jsx
@@ -1,16 +1,19 @@
 import React from "react";
 
 export const InputTodo = (props) => {
-  const { todoText, onChange, onClick } = props;
+  const { todoText, onChange, onClick, disabled } = props;
   return (
     <div className="input-area">
       <input
+        disabled={disabled}
         placeholder="TODOを入力"
         value={todoText}
         // onChangeを定義すると{}内の処理でevent引数が使える
         onChange={onChange}
       />
-      <button onClick={onClick}>追加</button>
+      <button disabled={disabled} onClick={onClick}>
+        追加
+      </button>
     </div>
   );
 };
