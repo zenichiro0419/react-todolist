092804b453ba3a1b675db7317d57f8298a37c810
Zenichiro Ishikawa <37606032+zenichiro0419@users.noreply.github.com>
2023-10-30 17:17:42 +0000

    入力欄と未完了リストの状態を変更

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,34 @@ import React, { useEffect, useState } from "react";
 import "./style.css";
 
 export const App = () => {
+  const [todoText, setTodoText] = useState("");
   const [incompleteTodos, setIncompleteTodos] = useState(["あああ", "いいい"]);
   const [completeTodos, setCompleteTodos] = useState(["ううう"]);
+
+  // 入力したときのフォーム欄に変更を加える。入力値の取り方はお決まりなので覚える。
+  const onChangeTodoText = (event) => setTodoText(event.target.value);
+  //　追加ボタンを押したときに未完了リストに追加
+  const onClockAdd = () => {
+    // 何も入力してないときは状態に変更を加えない
+    if (todoText === "") return;
+    // 未完了のtodoと入力したtodoを１つの配列に格納
+    const newTodos = [...incompleteTodos, todoText];
+    // incompleteTodosの状態に変更を加える（setIncompleteTodos）＝未完了リストを更新
+    setIncompleteTodos(newTodos);
+    // todoTextの状態に変更を加える（setTodoText）＝入力値を初期化
+    setTodoText("");
+  };
+
   return (
     <>
       <div className="input-area">
-        <input placeholder="TODOを入力" />
-        <button>追加</button>
+        <input
+          placeholder="TODOを入力"
+          value={todoText}
+          // onChangeを定義すると{}内の処理でevent引数が使える
+          onChange={onChangeTodoText}
+        />
+        <button onClick={onClockAdd}>追加</button>
       </div>
 
       <div className="incomplete-area">
